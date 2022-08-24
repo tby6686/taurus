15363b0d8a975d5343ae82b6b39c8f63c043c356
tby6686 <tby6686@163.com>
2022-08-24 11:12:52 +0800

    bug fix

--- taurus-common/src/main/java/cn/com/taurus/common/exception/user/UserException.java
+++ taurus-common/src/main/java/cn/com/taurus/common/exception/user/UserException.java
@@ -1,16 +1,13 @@
 package cn.com.taurus.common.exception.user;
 
-
 import cn.com.taurus.common.exception.BaseException;
-
 /**
  * 用户信息异常类
- * 
+ *
  * @author tby
  */
 public class UserException extends BaseException
 {
-    private static final long serialVersionUID = 1L;
 
     public UserException(String code, String defaultMsg, Object[] args)
     {
