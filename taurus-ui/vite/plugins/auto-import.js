e5c54ec9b844165cc5ed05e05efb39e4e9ca9846
tby6686 <tby6686@163.com>
2022-09-13 00:24:10 +0800

    bug fix

---
+++ taurus-ui/vite/plugins/auto-import.js
@@ -0,0 +1,15 @@
+/**
+ * https://github.com/antfu/unplugin-auto-import/
+ */
+import autoImport from 'unplugin-auto-import/vite'
+
+export default function createAutoImport() {
+    return autoImport({
+        imports: [
+            'vue',
+            'vue-router',
+            'pinia'
+        ],
+        dts: false
+    })
+}
---
+++ taurus-ui/vite/plugins/setup-extend.js
@@ -0,0 +1,7 @@
+/*
+import setupExtend from 'vite-plugin-vue-setup-extend'
+
+export default function createSetupExtend() {
+    return setupExtend()
+}
+*/
